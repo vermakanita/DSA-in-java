3a4abe9d2133787f2c73b3166b01345d16ad1e2e
KANITA VERMA <vermakanita13@gmail.com>
2024-09-17 23:16:06 +0530

    Add files via upload

---
+++ TakeLength.java
@@ -0,0 +1,19 @@
+package Array;
+
+import java.util.Scanner;
+
+public class TakeLength {
+    public static void main(String args[]){
+        Scanner sc = new Scanner (System.in);
+        System.out.println("enter length");
+        int n = sc.nextInt();
+        int arr[]= new int[n];
+        for ( int i =0 ; i<n ; i++){
+            arr[i]= sc.nextInt();
+        }
+        
+
+        
+
+    }
+}
