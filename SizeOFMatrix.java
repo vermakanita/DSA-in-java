c078fd81e93649a1694cd1eca9b305401209eb6e
KANITA VERMA <vermakanita13@gmail.com>
2024-09-19 22:48:09 +0530

    Add files via upload

---
+++ SizeOFMatrix.java
@@ -0,0 +1,34 @@
+package Array;
+import java.util.*;
+
+public class SizeOFMatrix {
+    public static void main(String args[]){
+        int [][]arr = new int [5][6];
+     arr[1][3]=10;
+     //get
+     System.out.println(arr[1][3]);
+     System.out.println(arr[1][3]);
+
+
+     //row
+     System.out.println(arr.length);
+
+     //col count
+     System.out.println(arr[0].length);
+
+     //traversal
+
+     for(int i =0; i<arr.length;i++){ // i->row no
+        for(int j=0;j<arr[0].length;j++){//j-col no
+
+
+            //processing
+
+        }
+
+     }
+
+
+
+     
+}}
